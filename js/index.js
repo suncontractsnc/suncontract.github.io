c0a5e59c7a3a53dda316751608a94cbc4ef2e61e
Luka <luka@migrant.local>
2017-05-26 02:46:42 +0200

    better?

---
+++ js/index.js
@@ -0,0 +1,25 @@
+﻿( function($) {
+$(document).ready(function () {
+   // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+        }
+      }
+    });
+});
+} ) ( jQuery );
