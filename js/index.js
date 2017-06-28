43284d93d742c69dc16a129fa1da3389174458fb
Luka <luka@pusic.com>
2017-06-28 18:02:05 +0200

    it's the final countdown

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@
 })();
 var Sun = {
     timer: null,
-    countdowndata: new Date(Date.UTC(2017, 05, 28, 16, 35, 0, 0)), //'Jun 28, 2017 8:00:00').getTime(), // end date
+    countdowndata: new Date(Date.UTC(2017, 05, 28, 16, 37, 0, 0)), //'Jun 28, 2017 8:00:00').getTime(), // end date
     init: function() {
         self = this;
         // Select all links with hashes
