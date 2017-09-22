1ccbf0c6316125e25716daf65ba4ad55410cd927
Gregor Spagnolo <gregor.spagnolo@gmail.com>
2017-09-22 11:25:46 +0200

    new site

---
+++ js/new_index.js
@@ -0,0 +1,16 @@
+var Index = {
+	init: function () {
+		$('.custom').click(function () {
+			$('.subscribe-us-margin').removeAttr('style');
+			$('.join-us-margin').css('display', 'none');
+        });
+        $('#mc-embedded-subscribe').click(function () {
+            $('.jojning_us').addClass('small_screen');
+            
+        });
+	}
+}
+
+$(document).ready(function () {
+	Index.init();
+});
